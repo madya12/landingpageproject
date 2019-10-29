44165625fe0d0ee6f148ab4ef68d5ab5bc42ffc9
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2019-10-29 22:26:48 +0700

    edit color and display movie

--- g-movie-3/themes/g-ultra/header.php
+++ g-movie-3/themes/g-ultra/header.php
@@ -57,7 +57,7 @@ include('functions.php'); ?>
   ?>
   <style type="text/css">
     .body-home {
-       background-image: url(<?php style_theme();?>/images/<?php echo $selectedBg; ?>); 
+        background-image: url(<?php style_theme();?>/images/<?php echo $selectedBg; ?>);
         background-repeat: no-repeat;
         background-size: cover;
         background-color: black;
@@ -158,7 +158,10 @@ include('functions.php'); ?>
         background-color: #3b599800!important;
         font-family: asap!important;
         font-weight: bold;
-    }    
+    }
+    .searchbox form {
+        padding-right: 0px!important;
+    }
     .btn {
         border-radius: 0px;
     }
@@ -171,10 +174,10 @@ include('functions.php'); ?>
         background: black;
     }
     .searchbox input[type=text] {
-        background: #000002;
-        border: 1px solid #0a0a0a;
+        background: #333;
+        border: 1px solid #333;
         float: left;
-        height: 20px;
+        height: 40px;
         width: 100%;
         border-radius: 5px;
         padding: 0 0px 0 8px;
@@ -229,7 +232,7 @@ include('functions.php'); ?>
         text-transform: uppercase;
     }
     .navbar-default .navbar-nav>li>a:hover {
-        color: #fff;
+        color: #333;
         font-size: 15px;
         font-weight: normal;
         text-transform: uppercase;
@@ -249,7 +252,7 @@ include('functions.php'); ?>
     }
     .releases h1 {
         margin-bottom: 0px!important;
-        background: #000000ed!important;
+        background: #fff!important;
         margin-top: 20px;
         color: #0096D6!important;
     }
@@ -311,7 +314,8 @@ include('functions.php'); ?>
         padding-top: 20px!important; 
         padding: 10px;
         text-align: center;
-    } 
+    }
+
 
     @media only screen and (max-width: 500px) {
         .searchbox form {
@@ -400,6 +404,12 @@ include('functions.php'); ?>
             padding-right: 0px;
             padding-left: 0px;
         }
+        .navbar-default .navbar-nav>li>a:hover {
+        color: #333;
+        font-size: 15px;
+        font-weight: normal;
+        text-transform: uppercase;
+    }
     }
 
 
@@ -471,16 +481,17 @@ include('functions.php'); ?>
 //<![CDATA[
 shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="/ops.php"});
 //]]>
-</script> <script type="text/javascript">
-//     function googleTranslateElementInit() {
-//       new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
-//   }
-// </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</script>
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+  }
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
 </head>
 
 <body class="body-home">
-    <!-- <div class="wrapper">
+    <div class="wrapper">
         <div class="container">
             <div class="">
                 <header class="style2">
@@ -505,7 +516,7 @@ shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!
                                                     <input type="text" name="s" placeholder="Search Here...">
                                                 </li>
                                                 <li class="pull-right">
-                                                    <input type="submit" value="GO">
+                                                    <input type="submit"  value="GO">
                                                 </li>
                                             </ul>
                                         </form>
@@ -514,25 +525,25 @@ shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!
                                 </div>
                                 <div class="col-lg-3 col-xs-12">
                                     <div id="google_translate_element" style="margin-top: 18px;"></div>
-                                </div> 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </header>
             </div>
-        </div>-->
+        </div>
         <div class="container menu-responsive">
-            <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navigationstrip bordercolor-top" style="margin-top: 20px; background-color: #fff;">
                 <div style="padding-left: 5px;">
                     <div class="row">
-                        <div class="col-lg-6 col-md-9 col-sm-6 col-xs-4">
+                        <div class="col-lg-10 col-md-9 col-sm-6 col-xs-4">
                             <div class="navigation">
-                                <div class="navbar yamm navbar-default ">
+                                <div class="navbar yamm navbar-default">
                                     <div class="row">
                                         <div class="navbar-header">
                                             <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle" aria-expanded="false"> <i class="fa fa-bars"></i> Menu </button>
                                         </div>
-                                        <div id="navbar-collapse-1" class="navbar-collapse collapse ddsmoothmenu navbar-fixed-top">
+                                        <div id="navbar-collapse-1" class="navbar-collapse collapse ddsmoothmenu">
                                             <ul class="nav navbar-nav">
                                                 <li><a href="<?php echo site_url() ?>"><i class="fa fa-home"></i> Home</a></li>
                                                 <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-video-camera"></i> Movies <span class="caret"></span></a>
@@ -558,28 +569,22 @@ shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!
                                                     </ul>
                                                 </li>
                                             </ul>
-                                        </div>                                        
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="clearfix"></div>
                             </div>
-                            
                         </div>
-                        <div class="col-lg-4 col-md-3 col-sm-6 col-xs-8 ">
-                                <div class="searchbox">
-                                    <form action="/" method="get">                                        
-                                            <li>
-                                                <input type="text" name="s" placeholder="Search Here...">                           
-                                                <input type="submit" class="btn-go" value="GO">                     
-                                            </li>                                                                                                                               
-                                    </form>
-                                </div>
-                        </div>                                
                         <div class="col-lg-2 col-md-3 col-sm-6 col-xs-8">
                           <div class="loginsec pull-right" style="a{color: black;}">
                             <a href="/?action=register&s2=59e4cf925fc61a59174d81ea&s3=59c4aa325fc61a765ec8715d"><i class="fa fa-sign-in"></i>Login / Register</a>
                         </div>
-                    </div>                  
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5 item">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- g-movie-3/themes/g-ultra/index.php
+++ g-movie-3/themes/g-ultra/index.php
@@ -11,15 +11,15 @@ include('header.php');
         <div class="container menu-responsive content-responsive">
             <div class="row"><div class="col-md-12 col-sm-12 col-xs-12">
     <!-- <h2 class="page-header"><i class="fa fa-film"></i> Movies</h2> -->
-    <div class="releases"><h1 style="color: #f29e37; border: 5px solid #0500040a; background: #000000a6;"><i class="fa fa-film"></i> Movies</h1></div>
-    <div style="background-color: #000000a3; width: 100%; overflow: hidden; padding-top: 15px;">
+    <div class="releases" ><h1 style="color: #f29e37; border: 5px solid #0500040a; background: #000000a6;"><i class="fa fa-film"></i> Movies</h1></div>
+    <div style="background-color: #000000a6; width: 100%; overflow: hidden; padding-top: 15px;">
                         <?php 
         if ( empty( $_GET['page'] ) ) { $page = 1; }else{ $page = $_GET['page']; }
         $Movies = unserialize( ocim_data_movie('home_m_',$page) );
         if( is_array($Movies['result']) ):
         foreach ( (array) array_slice($Movies['result'], 0, 12) as $row ) {
                 ?>
-                <div class="col-lg-2 col-md-3 col-sm-3 col-xs-6" itemscope itemtype="https://schema.org/Movie">
+                <div class="col-lg-4 col-md-5 col-sm-5 col-xs-12" itemscope itemtype="https://schema.org/Movie">
                         <div class="row">
                                 <figure class="gallery">
                                         <a href="<?php echo seo_movie($row['id'],$row['title']);?>" title="Watch <?php echo $row['title'];?> Full Movie" rel="bookmark" itemprop="url">
@@ -42,13 +42,12 @@ include('header.php');
 	        <?php 
                 }
         endif; 
-        ?>
-                
+        ?>                
                         </div>
         <div class="clearfix"></div><br>
         <!-- <h2 class="page-header"><i class="fa fa-film"></i> TV Shows</h2> -->
         <div class="releases"><h1 style="color: #f29e37; border: 5px solid #0500040a; background: #000000a6;"><i class="fa fa-film"></i> TV Shows</h1></div>
-    <div style="background-color: #000000a3; width: 100%; overflow: hidden; padding-top: 15px;">
+    <div style="background-color: #000000a6; width: 100%; overflow: hidden; padding-top: 15px;">
                         <?php 
         $TV = unserialize( ocim_data_tv('home_tv_',$page, 'getOnTheAirTVShows') );
         if( is_array($TV['result']) ):
