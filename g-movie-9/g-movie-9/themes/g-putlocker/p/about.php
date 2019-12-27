60cc53884c325662082a1059bb5b18b10970753f
Ahmad Shohibus Sulthoni <thoni.shohibus@gmail.com>
2019-12-27 18:57:33 +0700

    ica udah beres

---
+++ g-movie-9/g-movie-9/themes/g-putlocker/p/about.php
@@ -0,0 +1,98 @@
+<?php 
+$hack_title = 'About Us';
+$hack_description = "About we are.";
+get_header(); ?>
+<?php get_head(); ?>
+<div style="min-height:500px !important">
+   <div class="container p5" style="padding-top:20px !important; padding-bottom:20px !Important ">
+      <div class="" style="padding-top:10px; padding-bottom:10px;">
+         <a href="/" class="plpr" > Home </a> - 
+         <a class="plpr" > Info Footer </a>   - 	
+         <a  class="plpr" > About us.info </a>
+      </div>
+      <div style="background: rgba(0,0,0,0.3); color:white;" class=" p10 radius">
+         <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-12 p5">
+		 
+		<h3 class="border-bottom p10"> About us</h3> 
+		<br />
+		<p><strong>Why <?php echo config('sitename') ?>?</strong></p>
+<p>We Make Viewing Magic</p>
+<p>Watch the latest movies & TV shows anytime, anywhere - no subscriptions, no contract. Enjoy stunning quality in up to 4K UHD at home. Download and watch on your mobile device. Rent or own from over 100,000 titles, or watch thousands of movies free with <?php echo config('sitename') ?> Movies On Us.</p>
+<p> </p>
+<ul>
+<li>Watch on your favorite device.</li>
+<li>No subscriptions. No contract.</li>
+<li>Enjoy up to 4K UHD at home.</li>
+<li>Watch before Blu-ray, Netflix & Redbox.</li>
+</ul>
+<p> </p>
+<p><strong>Devices</strong></p>
+<p><?php echo config('sitename') ?> plays on all of your favorite devices</p>
+<p>Watch <?php echo config('sitename') ?> on smart TVs, connected Blu-ray players, game consoles (Xbox One/S/X, PlayStation 3 and 4), streaming devices (Apple TV, Roku, Chromecast etc.), Nvidia Shield, Android and iOS devices, Windows 10 devices, and more.</p>
+<p>Get your disc in the cloud</p>
+<p>Turn your Blu-ray or DVD collection into digital movies with Disc to Digital. Convert quickly and easily with the <?php echo config('sitename') ?> app on your mobile device – simply scan the UPC, convert, and watch from the cloud or download to your device. It’s not magic, it’s <?php echo config('sitename') ?>.</p>
+<p> </p>
+<p><strong>Movies On Us</strong></p>
+<p>Thousands of movies. Nothing to pay.</p>
+<p>Movies On Us lets you watch thousands of titles across countless genres for free. That’s right, free car chases, free roundhouse kicks, free moonlit walks, and so much more. So feel free to stop by and spend your free time with Movies On Us.</p>
+<p> </p>
+<ul>
+<li>Join us freely.<?php echo config('sitename') ?> Movies On Us - it’s free movies, with limited commercials.</li>
+</ul>
+<ul>
+<li>We have a lot to offer.Choose from thousands of free movies, in up to 1080p.</li>
+</ul>
+<ul>
+<li> Take us anywhere.Enjoy Movies On Us anywhere, anytime - on your TV, mobile, web & more.</li>
+</ul>
+<p> </p>
+<p><strong>Movie Lockers</strong></p>
+<p> </p>
+<p>Access all your movies from one place</p>
+<p>Get anytime, anywhere access to your movie collection when you connect <?php echo config('sitename') ?> and your favorite digital movie lockers.</p>
+<p> </p>		 
+		 
+	
+	</div>
+
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-12 p5">
+               <div class="p5">
+                  <a class="fs22" > Footer Info </a> 
+               </div>
+               <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5">
+                     <div class="row"style="background: rgba(0,0,0,0.3) !important; padding:10px; border-radius:3px; border-left:5px solid black !important">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5" >
+                           <a href="<?php echo view_page( 'contact-us' );?>" > Contact us </a><br />
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5">
+                     <div class="row"style="background: rgba(0,0,0,0.3) !important; padding:10px; border-radius:3px; border-left:5px solid black !important">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5" >
+                           <a href="<?php echo view_page( 'about' );?>" > About us </a><br />
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5">
+                     <div class="row"style="background: rgba(0,0,0,0.3) !important; padding:10px; border-radius:3px; border-left:5px solid black !important">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5" >
+                           <a href="<?php echo view_page( 'dmca-notice' );?>" > Terms of Service </a><br />
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5">
+                     <div class="row"style="background: rgba(0,0,0,0.3) !important; padding:10px; border-radius:3px; border-left:5px solid black !important">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12  p5" >
+                           <a href="<?php echo view_page( 'privacy-policy' );?>" > Privacy Policy </a><br />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<?php get_footer(); ?>
